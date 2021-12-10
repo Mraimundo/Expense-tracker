5631d592aa38ad781480ee782b239c818a121680
Mraimundo <fmdumundo18@gmail.com>
2021-12-10 17:14:29 -0300

    criando a função que filtra por ano e mês.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { useState, useEffect } from 'react';
+import { Item } from './types/Item';
+import { Category } from './types/Categories';
+import { categories } from './data/categories';
+import { items } from './data/items';
+
+import { filterListByMonth, getCurrentMonth } from './util/dataFilter';
+
+
+import { GlobalStyle } from "./styles/global";
+
+import * as S from './App.styles';
 
 export const App = () => {
-  return (
-    <div className="App">
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
 
-    </div>
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
+
+  return (
+    <>
+      <S.Container>
+        <S.Header>
+          <S.HeaderText>Sistema Financeiro</S.HeaderText>
+        </S.Header>
+        <S.Body>
+          ...
+        </S.Body>
+      </S.Container>
+      < GlobalStyle />
+    </>
   );
 }
 
---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  {
+    date: new Date(2021, 11, 10),
+    category: 'food',
+    title: 'McDonalds',
+    value: 32.12
+  },
+  {
+    date: new Date(2021, 11, 10),
+    category: 'food',
+    title: 'Burger King',
+    value: 28
+  },
+  {
+    date: new Date(2021, 11, 11),
+    category: 'rent',
+    title: 'Aluguel Apt',
+    value: 2300
+  },
+  {
+    date: new Date(2021, 12, 15),
+    category: 'Salary',
+    title: 'Salário ACME',
+    value: 4500
+  }
+]
