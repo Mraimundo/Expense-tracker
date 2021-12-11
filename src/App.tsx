a44c2bd3963ffc4161120f2b81fd589baa4b1e3b
Mraimundo <fmdumundo18@gmail.com>
2021-12-10 21:56:27 -0300

    terminando os components de InfoArea e TableArea

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import { items } from './data/items';
 
 import { filterListByMonth, getCurrentMonth } from './util/dataFilter';
 
-
+import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea';
 import { GlobalStyle } from "./styles/global";
 
 import * as S from './App.styles';
@@ -15,10 +16,34 @@ export const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+
+
 
   useEffect(() => {
     setFilteredList(filterListByMonth(list, currentMonth));
   }, [list, currentMonth]);
+
+  useEffect(() => {
+    let incomeCout = 0;
+    let expenseCout = 0;
+
+    for (let i in filteredList) {
+      if (categories[filteredList[i].category]?.expense) {
+        expenseCout += filteredList[i].value;
+      } else {
+        incomeCout += filteredList[i].value;
+      }
+    }
+
+    setIncome(incomeCout);
+    setExpense(expenseCout);
+  }, [filteredList]);
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
 
   return (
     <>
@@ -27,7 +52,13 @@ export const App = () => {
           <S.HeaderText>Sistema Financeiro</S.HeaderText>
         </S.Header>
         <S.Body>
-          ...
+          <InfoArea
+            currentMonth={currentMonth}
+            onMonthChange={handleMonthChange}
+            income={income}
+            expense={expense}
+          />
+          <TableArea list={filteredList} />
         </S.Body>
       </S.Container>
       < GlobalStyle />
