6251217146d505e803de90c9ef752bbcac9eff52
Mraimundo <fmdumundo18@gmail.com>
2021-12-13 15:59:11 -0300

    Refatorando os components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,11 @@
-import { useState, useEffect } from 'react';
-import { Item } from './types/Item';
-import { Category } from './types/Categories';
-import { categories } from './data/categories';
-import { items } from './data/items';
-
-import { filterListByMonth, getCurrentMonth } from './util/dataFilter';
-
-import { TableArea } from './components/TableArea';
-import { InfoArea } from './components/InfoArea';
-import { InsertArea } from './components/InsertArea';
 import { GlobalStyle } from "./styles/global";
 
-import * as S from './App.styles';
+import { Home } from './page/home';
 
 export const App = () => {
-  const [list, setList] = useState(items);
-  const [filteredList, setFilteredList] = useState<Item[]>([]);
-  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
-  const [income, setIncome] = useState(0);
-  const [expense, setExpense] = useState(0);
-
-
-
-  useEffect(() => {
-    setFilteredList(filterListByMonth(list, currentMonth));
-  }, [list, currentMonth]);
-
-  useEffect(() => {
-    let incomeCout = 0;
-    let expenseCout = 0;
-
-    for (let i in filteredList) {
-      if (categories[filteredList[i].category]?.expense) {
-        expenseCout += filteredList[i].value;
-      } else {
-        incomeCout += filteredList[i].value;
-      }
-    }
-
-    setIncome(incomeCout);
-    setExpense(expenseCout);
-  }, [filteredList]);
-
-  const handleMonthChange = (newMonth: string) => {
-    setCurrentMonth(newMonth);
-  }
-
-  const handleAddItem = (item: Item) => {
-    let newList = [...list];
-    newList.push(item);
-    setList(newList);
-  }
-
   return (
     <>
-      <S.Container>
-        <S.Header>
-          <S.HeaderText>Sistema Financeiro</S.HeaderText>
-        </S.Header>
-        <S.Body>
-          <InfoArea
-            currentMonth={currentMonth}
-            onMonthChange={handleMonthChange}
-            income={income}
-            expense={expense}
-          />
-
-          <InsertArea
-            onAdd={handleAddItem}
-          />
-
-          <TableArea list={filteredList} />
-        </S.Body>
-      </S.Container>
+      <Home />
       < GlobalStyle />
     </>
   );
